5f237de56ca40b3a7070bea252891c9cb3a0ee14
nhuttan <23644821.tan@student.iuh.edu.vn>
2024-08-11 17:26:09 +0700

    message

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome  {
+    public static void main(String[] args) {
+        System.out.println("Hello Nhut Tan!");
+        System.out.println("Welcome to the DHKTPM19ATT!");
+    }
+}
